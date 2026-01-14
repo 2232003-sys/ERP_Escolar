c93a8bfa3a67db46dfe84a45196686382e20ec17
Israel Morales <israel.erik.m@gmail.com>
2026-01-13 20:40:00 -0600

    FIX FINAL: Base de datos conectada y funcionando correctamente

---
+++ ERPEscolar.API/Models/Alumno.cs
@@ -0,0 +1,9 @@
+namespace ERPEscolar.API.Models;
+
+public class Alumno
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public string? Apellido { get; set; }
+    public string? Email { get; set; }
+}
--- ERPEscolar.API/Program.cs
+++ ERPEscolar.API/Program.cs
@@ -1,9 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using ERPEscolar.API.Data;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configurar Entity Framework Core
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
